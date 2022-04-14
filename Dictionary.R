4ff1d99baf69c70d563132d27328563b60919e50
Natalia Savytska <43849297+savytskanatalia@users.noreply.github.com>
2022-04-14 16:41:29 +0200

    Update Dictionary.R

--- Dictionary.R
+++ Dictionary.R
@@ -1,7 +1,5 @@
 library(data.table)
 library(stringr)
-
-
 all_genes_ann<-fread( 'gencode_v29_lncpedia_GenesOnly_ENSG.gtf' , select=c(9), sep="\t")
 all_genes_ann <- unique(all_genes_ann$V9[all_genes_ann$V9 %like% "ENSG"])
 all_genes_ann<-str_split_fixed(all_genes_ann, "; ",n=12)
@@ -55,5 +53,18 @@ all_genes_ann$V3<-gsub("\\..*","",all_genes_ann$V3)
 all_genes_ann[all_genes_ann$V1=="DLEU1",2]<-"ENSG00000176124"
 all_genes_ann<-all_genes_ann[,1:2]
 # Now dictionary for elements where V1 is not ENSG ID
-fwrite(all_genes_ann, 'LNCID_to_ENSG_dictionary.txt',sep="\t",quote = F)
+colnames(all_genes_ann)<-c("ID","ENSEMBL")
+# Add also IDs missing
+all_genes_ann<-all_genes_ann[!(all_genes_ann$ID %like% "ENSG"),]
+add_df<-data.frame(ID=c("HCG23","LINC00678","lnc-PIWIL4-1","lnc-SNRPN-1","P3H2-AS1","LNCPRESS2","LINC01950","SNHG4","lnc-STK32A-3","PVT1","lnc-PRAG1-3","lnc-DIAPH2-16",
+                        "OLMALINC","lnc-SLC38A2-1","lnc-PCED1B-5","GAU1","lnc-PCDH8-10","G2E3-AS1","lnc-FAM103A1-2","lnc-SHISA9-3","lnc-CDH5-3","lnc-DHX38-3","lnc-VSTM2B-5",
+                        "lnc-A1BG-1","lnc-MYO7B-2","lnc-RBM11-2","LINC01424","LINC01487","LINC02211","CKMT2-AS1","CASC15","lnc-KLHL32-4","lnc-ARL4A-54","lnc-KCNB2-12","FIRRE"),
+                   ENSEMBL=c("ENSG00000228962","ENSG00000254934","ENSG00000255666","ENSG00000279192","ENSG00000225764","ENSG00000249152","ENSG00000251027","ENSG00000281398","ENSG00000280780",
+                             "ENSG00000249859","ENSG00000253893"," ENSG00000281566","ENSG00000235823","ENSG00000257496","ENSG00000272369","ENSG00000255474","ENSG00000274090",
+                             "ENSG00000257636","ENSG00000260608","ENSG00000262801","ENSG00000260364","ENSG00000260664","ENSG00000267498","ENSG00000268516","ENSG00000231731",
+                             "ENSG00000224905","ENSG00000236519","ENSG00000241336","ENSG00000245662","ENSG00000247572","ENSG00000272168","ENSG00000271860","ENSG00000229618",
+                             "ENSG00000254277","ENSG00000213468"))
 
+all_genes_ann<-rbind(all_genes_ann,add_df)
+
+fwrite(all_genes_ann, 'LNCID_to_ENSG_dictionary.txt',sep="\t",quote = F)
